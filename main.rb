0d11a7006f437c510c56cbd03a7215d22231b2f1
abvdasker <hal.c.rogers@gmail.com>
2015-12-01 22:27:17 -0500

    Begins js app

--- main.rb
+++ main.rb
@@ -1,8 +1,13 @@
 require "sinatra"
+require "json"
 
 set :base, File.dirname(__FILE__)
 set :public_folder, settings.base + "/static"
 
 get "/" do
   File.new(settings.public_folder + "/index.html")
+end
+
+post "/users" do
+
 end
--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,24 @@
-window.onload = function() {
-  alert("Hello World!");
-}
+var PeerChat = (function() {
+  var me;
+  var init = function() {
+    var $userNameButton = $("#set-user-name");
+    $userNameButton.on("click", createUserClick);
+  }
+
+  var createUserClick = function(e) {
+    var newUsername = $("#user-name").val();
+    $.post("/users", { username: newUsername }, createUserSuccess, "json");
+  }
+
+  var createUserSuccess = function(data) {
+    $("#set-user-name").disable();
+    $("user-name").disable();
+    me = new User(data["username"]);
+  }
+
+  return {
+    init: init
+  }
+})();
+
+window.onload = PeerChat.init;
