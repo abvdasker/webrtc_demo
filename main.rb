6b3d20fdef3beb877654cc02b86d682e01418203
abvdasker <hal.c.rogers@gmail.com>
2015-12-01 22:00:03 -0500

    Begin properly rendering static file

--- main.rb
+++ main.rb
@@ -1,5 +1,8 @@
 require "sinatra"
 
+set :base, File.dirname(__FILE__)
+set :public_folder, settings.base + "/static"
+
 get "/" do
-  "Hello World!"
+  File.new(settings.public_folder + "/index.html")
 end
