1cc48b01d1f861ae4cb24e95f5fe4e28998245a9
abvdasker <hal.c.rogers@gmail.com>
2015-12-02 00:25:07 -0500

    Working websockets broadcast

--- main.rb
+++ main.rb
@@ -1,13 +1,64 @@
 require "sinatra"
+require "sinatra-websocket"
 require "json"
+require "pry"
 
+set :server, 'thin'
 set :base, File.dirname(__FILE__)
 set :public_folder, settings.base + "/static"
+set :users, {}
+set :sockets, {}
+
+class User
+  attr_reader :username
+  attr_accessor :socket
+
+  def initialize(username)
+    @username = username
+  end
+
+  def to_json(options = {})
+    {
+      username: username
+    }.to_json
+  end
+end
+
+def broadcast_users
+  users = settings.users.values
+  users.each do |user|
+    user.socket.send((users - [user]).to_json)
+  end
+end
 
 get "/" do
   File.new(settings.public_folder + "/index.html")
 end
 
 post "/users" do
+  content_type :json
+  user_json = JSON.parse(request.body.read)
+  user = User.new(user_json["username"])
+  unless settings.users[user.username]
+    settings.users[user.username] = user
+  end
+  user.to_json
+end
 
+get "/users" do
+  request.websocket do |ws|
+    ws.onmessage do |msg|
+      user_json = JSON.parse(msg)
+      current_user = settings.users[user_json["username"]]
+      current_user.socket = ws
+      settings.sockets[ws] = current_user
+      EM.next_tick { broadcast_users }
+    end
+    ws.onclose do
+      current_user = settings.sockets[ws]
+      settings.sockets.delete(ws)
+      settings.users.delete(current_user.username)
+      EM.next_tick { broadcast_users }
+    end
+  end
 end
--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,8 @@
+var User = function(username) {
+  this.username = username;
+}
+
 var PeerChat = (function() {
-  var me;
   var init = function() {
     var $userNameButton = $("#set-user-name");
     $userNameButton.on("click", createUserClick);
@@ -7,13 +10,40 @@ var PeerChat = (function() {
 
   var createUserClick = function(e) {
     var newUsername = $("#user-name").val();
-    $.post("/users", { username: newUsername }, createUserSuccess, "json");
+    $.post("/users", JSON.stringify({ username: newUsername }), createUserSuccess, "json");
   }
 
   var createUserSuccess = function(data) {
-    $("#set-user-name").disable();
-    $("user-name").disable();
-    me = new User(data["username"]);
+    var me = new User(data["username"]);
+    $("#set-user-name").remove();
+    $("#user-name").remove();
+    $("#set-user-name-label").remove();
+    $("body").prepend("<h2>Welcome " + me.username + "</h2>");
+    $("#main-app").removeClass("hidden");
+    startWebSocket(me);
+  }
+
+  var startWebSocket = function(me) {
+    var socket = new WebSocket("ws://127.0.0.1:4567/users");
+    socket.onopen = function() {
+      console.log("connection open");
+      socket.send(JSON.stringify(me));
+      console.log("getting users...");
+    }
+    socket.onmessage = function(msg) {
+      userList = JSON.parse(msg.data);
+      updateUsers(userList);
+      console.log(userList);
+    }
+  }
+
+  var updateUsers = function(userList) {
+    var $userList = $("#user-list");
+    $userList.empty();
+    for (var i in userList) {
+      var user = userList[i];
+      $userList.append("<div>" + user.username + "</div>");
+    }
   }
 
   return {
